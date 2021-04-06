941ee11f8a3b082bb51912828a7bc82f80c66822
OyugoObonyo <nyikal1999@gmail.com>
2021-04-06 18:17:01 +0300

    Create a manifest with content

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Creates Puppet file with content
+
+file {'/tmp/holberton':
+    path    => '/tmp/holberton',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+}
