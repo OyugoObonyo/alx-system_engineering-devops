ccab2212221b0dea7edc52c2d554c05e8341ce88
OyugoObonyo <nyikal1999@gmail.com>
2021-05-03 17:55:00 +0300

    Gather information of particular employee

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+import requests
+import json
+import sys
+
+
+def getInformation(employeeid):
+    """Returns information based on ID"""
+    url = "https://jsonplaceholder.typicode.com/"
+    endpoint = url + 'users/{}'.format(employeeid)
+    employee = requests.get(endpoint).json()
+    taskendpoint = url + 'TODOs?userId={}'.format(employee.get('id'))
+    tasks = requests.get(taskendpoint).json()
+    data = {"employee": employee, "tasks": tasks}
+    totalTasks = len(data['tasks'])
+    tasks = [task for task in data['tasks'] if task['completed']]
+    completedTasks = len(tasks)
+    print('Employee {} is done with tasks({}/{}):'
+          .format(data['employee']['name'], completedTasks, totalTasks))
+    for task in tasks:
+        print('\t {}'.format(task['title']))
+
+
+if __name__ == '__main__':
+    getInformation(sys.argv[1])
